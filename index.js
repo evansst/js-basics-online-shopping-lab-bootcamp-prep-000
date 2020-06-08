78d601b8b52e6072a1b4c4e9641f1d296774a469
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 21:35:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function total() {
 
 function removeFromCart(item) {
   var i = 0;
-  while (i <= cart.length) {
+  while (i+1 < cart.length) {
     if (cart[i].itemName === item) {
       var removed = cart.splice(i,1);
       return cart;
