44a294f9cd3a9b83e3094afb525f5aeca07167bf
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 21:04:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,26 +17,26 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0) {                                                   //check if the cart is empty
-    return `Your shopping cart is empty.`;                                   //return string if the cart is empty
-  } else {                                
-    var i = 0;                                                              // If cart is NOT empty, declare iteration var and output
+  if (cart.length === 0) {                                                        //check if the cart is empty
+    return `Your shopping cart is empty.`;                                        //return string if the cart is empty
+  } else {                                  
+    var i = 0;                                                                   // If cart is NOT empty, declare iteration var and output
     var output = `In your cart, you have `;
-    if (cart.length === 1){
+    if (cart.length === 1){                                                      //If 1 item in cart, add item and price to output and return  
       output = output + `${cart[i].itemName} at $${cart[i].itemPrice}.`;
     } else {
-    while (i+1 < cart.length) {
+    while (i+1 < cart.length) {                                                   //if 2 or emore, add items until last item
       output = output + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
       i += 1;
     }
-    output = output + `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+    output = output + `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;        //add last item to output string
   }
-  return output;
+  return output;                                                                  //return output string
 }
 }
 
 function total() {
-  // write your code here
+  
 }
 
 function removeFromCart(item) {
