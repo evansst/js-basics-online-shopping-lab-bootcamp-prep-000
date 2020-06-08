ccf296a8a0ad0a0a776aa2189247ba3573215bfc
thoughtful-bundle-9618 <evansst@icloud.com>
2020-06-08 21:01:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,15 +6,33 @@ function getCart() {
 
 function setCart(c) {
   cart = c;
-  return cart;
+  return cart;                                                  
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = new Object ({itemName: item ,
+                    itemPrice: Math.floor(Math.random()*100)});           //create the new item, with a ranom price between 1 and 100
+  cart.push(newItem);                                                     //push the new item on to the array
+  return `${newItem.itemName} has been added to your cart.`;              //return string output
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {                                                   //check if the cart is empty
+    return `Your shopping cart is empty.`;                                   //return string if the cart is empty
+  } else {                                
+    var i = 0;                                                              // If cart is NOT empty, declare iteration var and output
+    var output = `In your cart, you have `;
+    if (cart.length === 1){
+      output = output + `${cart[i].itemName} at $${cart[i].itemPrice}.`;
+    } else {
+    while (i+1 < cart.length) {
+      output = output + `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+      i += 1;
+    }
+    output = output + `and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+  }
+  return output;
+}
 }
 
 function total() {
